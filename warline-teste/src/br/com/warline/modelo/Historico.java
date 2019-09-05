349fbbfa04867cbf9458a0815bdbb5a324dd6a46
adrian0silva <adrianosilvaparola@gmail.com>
2019-09-05 10:53:16 -0300

    adicionando lista de lista de jogadas e transformando em uma lista simples

--- warline-teste/src/br/com/warline/modelo/Historico.java
+++ warline-teste/src/br/com/warline/modelo/Historico.java
@@ -3,10 +3,14 @@ package br.com.warline.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.w3c.dom.ls.LSInput;
+
 public class Historico {
 	
 	private List<Jogador> jogadores = new ArrayList<>();
 	private List<Estado> estados = new ArrayList<>();
+	
+	private List<List<Jogada>> listaDaListaDeJogadas = new ArrayList<List<Jogada>>();
 	
 	private Integer round = 0;
 	
@@ -15,9 +19,17 @@ public class Historico {
 
 
 	public void jogar() {
-		
+		for(int a = 0;a < this.jogadores.size();a++) {			
+			this.listaDaListaDeJogadas.add(this.jogadores.get(a).getJogadas());
+		}
 		
 		round++;
+	}
+	
+	public List<Jogada> getListaDaListaDeJogadas() {
+		List<Jogada> jogadas = new ArrayList<Jogada>();
+		this.listaDaListaDeJogadas.forEach(e -> jogadas.addAll(e));
+		return jogadas;
 	}
 	
 	public Integer getRound() {
