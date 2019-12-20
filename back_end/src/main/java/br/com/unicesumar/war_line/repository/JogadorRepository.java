79cce30c96f5dc94190e4c07586f333b2d3a0bad
adrian0silva <adrianosilvaparola@gmail.com>
2019-12-19 22:55:29 -0200

    deletado projeto

--- back_end/src/main/java/br/com/unicesumar/war_line/repository/JogadorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.unicesumar.war_line.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.unicesumar.war_line.modelo.Jogador;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Long>{
-
-}
--- back_end/src/main/java/br/com/unicesumar/war_line/repository/JogoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.unicesumar.war_line.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.unicesumar.war_line.modelo.Jogo;
-
-public interface JogoRepository extends JpaRepository<Jogo, Integer>{
-
-}
